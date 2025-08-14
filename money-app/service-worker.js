a0a37e1f223ee742152b87fe35575b79a0a1d4bf
IH01SONG <ammmmmmma1@gmail.com>
2025-08-14 16:54:33 +0900

    sa

--- money-app/service-worker.js
+++ money-app/service-worker.js
@@ -1,17 +1,18 @@
 // service-worker.js
 
 const CACHE_NAME = "money-app-cache-v1";
+// GitHub Pages 같은 서브경로(host/reading-app/money-app/) 배포를 고려해 상대경로 사용
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/main.js",
-  "/js/expense.js",
-  "/js/todo.js",
-  "/js/charts.js",
-  "/images/icons/icon-192x192.png",
-  "/images/icons/icon-512x512.png",
-  "/manifest.json",
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/main.js",
+  "./js/expense.js",
+  "./js/todo.js",
+  "./js/charts.js",
+  "./images/icons/icon-192x192.png",
+  "./images/icons/icon-512x512.png",
+  "./manifest.json",
 ];
 
 self.addEventListener("install", (event) => {
